74a37c3b3d09ae15682634cae4bbcbdd47b1a649
shahabuddin <shahabuddin@firstfintech.com>
2025-12-01 10:33:41 +0000

    Add config  so that we have less line of code in jenkins pipeline

---
+++ jenkins-config.groovy
@@ -0,0 +1,13 @@
+SERVICES = [
+    "frontend": "src/frontend",
+    "adservice": "src/adservice", 
+    "cartservice": "src/cartservice/src",
+    "checkoutservice": "src/checkoutservice",
+    "currencyservice": "src/currencyservice",
+    "emailservice": "src/emailservice",
+    "paymentservice": "src/paymentservice",
+    "productcatalogservice": "src/productcatalogservice",
+    "recommendationservice": "src/recommendationservice",
+    "shippingservice": "src/shippingservice",
+    "loadgenerator": "src/loadgenerator"
+]
